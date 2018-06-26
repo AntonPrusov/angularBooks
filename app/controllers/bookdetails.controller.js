c0848ac552496817b4cf51c2dcfd9e7320e065af
anton <antonprusov2008@gmail.com>
2018-06-26 22:15:23 +0300

    2

--- app/controllers/bookdetails.controller.js
+++ app/controllers/bookdetails.controller.js
@@ -3,22 +3,53 @@
     'use strict';
 
     app.controller('BookDetails', ['$scope', 'books.repository', '$routeParams', function ($scope, booksRepository, $routeParams)
-            {
+            {   
+                var bookModel = {};
+
                 booksRepository.getBookById($routeParams.id)
                 .then(function(response) {
                     $scope.bookDetails = response.data;
+                    bookModel = angular.copy($scope.bookDetails);
                     $scope.bookDetails.date = new Date($scope.bookDetails.date);
-                    console.log($scope.bookDetails);
                 }, function(error) {
                     alert(error)}
                     );
 
+                booksRepository.getAuthors()
+                .then(function(response) {
+                    $scope.authors = response.data.map(function(item) {
+                        return {
+                            id: item.id,
+                            name: item.firstname + ' ' + item.lastname
+                        }
+                    });
+                }, function(error) {
+                    alert(error)}
+                    );
+
+
                 $scope.isEditable = false;
+
                 $scope.edit = function() {
                     $scope.isEditable = true;
                 }
+
                 $scope.cancelEdit = function() {
+                    $scope.bookDetails = angular.copy(bookModel);
                     $scope.isEditable = false;
+                }
+
+                $scope.save = function() {
+                    booksRepository.updateBookById($scope.bookDetails.id, $scope.bookDetails);
+                    $scope.isEditable = false;
+                }
+
+                $scope.getAuthorNameById = function(id) {
+                    if(!$scope.authors || !id) return;
+
+                    return $scope.authors.filter(function(item) {
+                        return item.id === id;
+                    })[0].name
                 }
             }
         ]);
--- app/services/books.factory.js
+++ app/services/books.factory.js
@@ -1,22 +1,27 @@
 (function() {
+    'use strict';
+    app.factory('books.repository', ['webApi', '$http', function(webApi, $http) {
+        return {
+            getBooks: _getBooks,
+            getBookById: _getBookById,
+            getAuthors: _getAuthors,
+            updateBookById: _updateBookById
+        };
 
-'use strict';
+        function _getBooks() {
+            return $http.get(webApi.DOMAIN + '/api/v2/books');
+        }
 
-app.factory('books.repository', ['webApi', '$http', function(webApi, $http) {
-	return {
-		getBooks: _getBooks,
-		getBookById: _getBookById,
-		//updateBookById: _updateBookById
-	};
+        function _getBookById(id) {
+            return $http.get(webApi.DOMAIN + '/api/v2/books/' + id);
+        }
 
-	function _getBooks() {
-		return $http.get(webApi.DOMAIN + '/api/v2/books');
-	}
+        function _getAuthors() {
+            return $http.get(webApi.DOMAIN + '/api/v2/authors');
+        }
 
-	function _getBookById(id) {
-		return $http.get(webApi.DOMAIN + '/api/v2/books/' + id);
-	}
-
-}]);
-
+        function _updateBookById(id, data) {
+        	return $http.put(webApi.DOMAIN + '/api/v2/books/' + id, data);
+        }
+    }]);
 })();
